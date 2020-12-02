8e871dfec37e559f2472a348c6c2ecfd2fcda659
Sarvesh <r.srisarvesh@gmail.com>
2020-12-02 16:00:50 +0530

    After catogorizing the datas into the countries

--- CreateMap.py
+++ CreateMap.py
@@ -13,7 +13,19 @@ def load_csv_file(csv_file):
 def create_map(csv_file, output_html):
     mood_content = load_csv_file(csv_file) #Loading the csv file into the mood_content
 
-    print(mood_content[0])
+    #Classify each mood_content item in country locations
+    mood_location={}     #We are declaring a dictionary that 
+    for item in mood_content:
+        if item['location'] not in mood_location:
+            mood_location[item['location']]={'Positive':0,'Negative':0}
+        mood_location[item['location']][item['mood']]+=1   #for each location the mood is counted according to its occurances
+
+    cnt=0
+    for item in mood_location:  #from the dictionary we are taking the country name and along with that we print how many positive and negative
+        print(item,mood_location[item])
+        cnt+=1
+        if cnt>10:
+            break
 
 
 if __name__ == "__main__":
