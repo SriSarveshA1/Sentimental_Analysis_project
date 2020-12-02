529e59f12e33dfbd0a19f3e64261df13a6101476
Sarvesh <r.srisarvesh@gmail.com>
2020-12-02 11:08:38 +0530

    After geting the location of tweets by using the geopy.exc and passing the location_name from the user object of the twiter object

--- ProcessTwitterDump.py
+++ ProcessTwitterDump.py
@@ -4,6 +4,8 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.tag import pos_tag
+from geopy.exc import GeocoderTimedOut #geopy makes easy for developers to locate coordinates of addresses,cities,countries across the globe using the third party geocoders
+from geopy.geocoders import Nominatim
 
 
 class MoodClassifier:
@@ -48,6 +50,27 @@ class MoodClassifier:
         return category
 
 
+class Locator:
+    def __init__(self):
+        self.geo_locator=Nominatim(user_agent="LearnPython") #User_Agent is an http request header that is sent with each request...#Nominatim requires this value to be set to your application name..M        self.geo_locator=Nominatim(user_agent="LearnPython") #User_Agent is an http request header that is sent with each request...#Nominatim requires this value to be set to your application name..M        self.geo_locator=Nominatim(user_agent="LearnPython") #User_Agent is an http request header that is sent with each request...#Nominatim requires this value to be set to your application name..
+                                                             #Moreover, should you have a lot of queries, Nominatim asks that the user_agent also contains your email address (or that you use the email parameter)
+        self.location_store={}
+        self.lookups=0
+
+    def get_location(self,location_name):
+        if location_name in self.location_store:   #If the location_name is already there in the location_store we return the value of location_name from the location_store dictionary
+            return self.location_store[location_name]
+        try:
+            self.lookups+=1   #Each time we are finding the location we are adding the lookups
+            location=self.geo_locator.geocode(location_name,language='en')
+            self.location_store[location_name]=location
+        except GeocoderTimedOut:   #when the time taken to find the location went more we need to return None
+            location=None
+        return location
+
+
+
+
 def process(input_file, output_file):
     tweets = None
     with open(input_file) as f:
@@ -56,15 +79,23 @@ def process(input_file, output_file):
     print("Number of tweets", len(tweets))
 
     classifier=MoodClassifier()
+    locator=Locator()
     cnt=0
     stat={'Positive':0,'Negative':0}
     for tweet in tweets:
         if 'retweeted_status' in tweet:
             tweet=tweet['retweeted_status']
         stat[classifier.get_mood(tweet['full_text'])]+=1
+
+        if 'location' in tweet['user']:            #Every tweet has a user object inside which there is and location object which holds the location object
+            location=locator.get_location(tweet['user']['location'])  #we pass the location name
+            if location:
+                print(location.address)
+                print(location.latitude, location.longitude)
+
         cnt+=1
         if cnt > 10:
-            pass
+            break
     print(stat)
     print("The mood is",stat['Positive']/(stat['Positive']+ stat['Negative']))
 
